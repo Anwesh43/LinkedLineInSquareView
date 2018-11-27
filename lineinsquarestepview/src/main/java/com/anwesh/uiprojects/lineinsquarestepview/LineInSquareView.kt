ef57400c3dcd636f2475ef41d49be6028f617cb4
Anwesh43 <anweshthecool0@gmail.com>
2018-11-27 18:06:15 +0530

    adding Float and Int canvas methods

--- lineinsquarestepview/src/main/java/com/anwesh/uiprojects/lineinsquarestepview/LineInSquareView.kt
+++ lineinsquarestepview/src/main/java/com/anwesh/uiprojects/lineinsquarestepview/LineInSquareView.kt
@@ -17,3 +17,13 @@ val lines : Int = 4
 val scDiv : Double = 0.51
 val scGap : Float = 0.05f
 val color : Int = Color.parseColor("#283593")
+
+fun Int.getInverse()  : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.valueFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1f - valueFactor()) * a.getInverse() + valueFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = scGap * dir * mirrorValue(a, b)
