3f523ecad30a6b8fd7bcec17fc3a7814ce58dc6f
Anwesh43 <anweshthecool0@gmail.com>
2018-11-27 18:40:56 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlineinsquarestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlineinsquarestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlineinsquarestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.lineinsquarestepview.LineInSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : LineInSquareView = LineInSquareView.create(this)
     }
 }
