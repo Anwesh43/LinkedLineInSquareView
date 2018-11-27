859e75ffc49dcb27914e0d2080c170e6442261c0
Anwesh43 <anweshthecool0@gmail.com>
2018-11-27 18:33:51 +0530

    adding LISNode class

--- lineinsquarestepview/src/main/java/com/anwesh/uiprojects/lineinsquarestepview/LineInSquareView.kt
+++ lineinsquarestepview/src/main/java/com/anwesh/uiprojects/lineinsquarestepview/LineInSquareView.kt
@@ -76,7 +76,7 @@ class LineInSquareView(ctx : Context) : View(ctx) {
 
     data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
 
-        fun update(cb : (Float, Int) -> Unit) {
+        fun update(cb : (Float) -> Unit) {
             scale += scale.updateScale(dir, lines, 1)
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
@@ -116,8 +116,51 @@ class LineInSquareView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class LISNode(var i : Int, val state : State = State()) {
+        private var next : LISNode? = null
+        private var prev : LISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLISNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LISNode {
+            var curr : LISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
