a878aae68bb1000d71296c34b625beaa1c9fa701
Anwesh43 <anweshthecool0@gmail.com>
2018-11-27 17:40:58 +0530

    adding libraries and defining constants

--- lineinsquarestepview/src/main/java/com/anwesh/uiprojects/lineinsquarestepview/LineInSquareView.kt
+++ lineinsquarestepview/src/main/java/com/anwesh/uiprojects/lineinsquarestepview/LineInSquareView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.lineinsquarestepview
 /**
  * Created by anweshmishra on 27/11/18.
  */
+
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val color : Int = Color.parseColor("#283593")
